9d569cfcf03a0820674c2abdf38b7a4143b10a8e
Ralf Hirning <ralf.hirning@provinzial.de>
2024-10-17 03:02:17 +0200

    jdk 11, vaadin 24.4.13, nicki 11.36

--- nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/auth/BasicAuthAdapter.java
+++ nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/auth/BasicAuthAdapter.java
@@ -10,9 +10,9 @@ package org.mgnl.nicki.vaadin.base.auth;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,19 +21,13 @@ package org.mgnl.nicki.vaadin.base.auth;
  * #L%
  */
 
-
 import org.mgnl.nicki.core.auth.SSOAdapter;
 
-import com.vaadin.flow.server.VaadinServletRequest;
-
-
 public class BasicAuthAdapter extends org.mgnl.nicki.core.auth.BasicAuthAdapter implements SSOAdapter {
-	public Object getRequest() {
-		if (super.getRequest() instanceof VaadinServletRequest) {
-			return ((VaadinServletRequest) super.getRequest()).getHttpServletRequest();
-		} else {
-			return super.getRequest();
-		}
-	}
-	
+	/*
+	 * public Object getRequest() { if (super.getRequest() instanceof
+	 * VaadinServletRequest) { return ((VaadinServletRequest)
+	 * super.getRequest()).getHttpServletRequest(); } else { return
+	 * super.getRequest(); } }
+	 */
 }
